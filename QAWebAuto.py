d1811fe45c151ebe432b1db9c9af294a30145dff
kevinRD19 <kevin.reimundo@hotmail.com>
2023-02-12 19:11:31 +0100

    comments + PEP8

--- QAWebAuto.py
+++ QAWebAuto.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import unittest
 from datetime import datetime
@@ -11,108 +9,155 @@ from datetime import datetime
 
 class AutoWebTests(unittest.TestCase):
     browser = None
+
+    '''
+    Method to set up the browser and the page to test
+    '''
     def setUp(self):
+        # Create a new Chrome session
         options = webdriver.ChromeOptions()
         options.add_argument('--start-maximized')
         self.browser = webdriver.Chrome(options=options)
+        # Navigate to the INNOCV home page
+        self.browser.get('https://www.innocv.com')
         self.addCleanup(self.browser.quit)
 
+    '''
+    Test for the first case
+    '''
     def test1(self):
-        self.browser.get('https://www.innocv.com')
+        # Accept cookies
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.ID, "rcc-decline-button")
         )).click()
 
+        # Go to the contact page
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.LINK_TEXT, "Contacto")
         )).click()
 
-        num_telephone = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(
-            (By.XPATH,'/html/body/div/div/div/div[1]/div/div[1]/div/div/div/div/p[2]/span/span[2]')
+        # Get the phone number
+        num = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div/div/div/div[1]/div/div[1]/div/div/div/div/p[2]/span/span[2]')
         )).text[6:]
 
-        aviso = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((
-            By.LINK_TEXT, "AVISO LEGAL")))
+        # Get the legal notice link page
+        aviso = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(
+            (By.LINK_TEXT, "AVISO LEGAL")
+        ))
+        # Go to the legal notice link position
         self.browser.execute_script("arguments[0].scrollIntoView();", aviso)
         sleep(1)
+        # Wait for the legal notice link to be present and click it
         aviso = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(
             (By.XPATH, "//a[.//li[text()='AVISO LEGAL']]")
         ))
         self.browser.execute_script("arguments[0].click();", aviso)
 
+        # Accept cookies
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.ID, "rcc-decline-button")
         )).click()
-        num_telephone2 = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((
-            By.XPATH, f"//*[contains(text(),'Teléfono')]"))).text.split('Teléfono ')[1]\
-                .split('.')[0]
-        self.assertEqual(num_telephone, num_telephone2)
+        # Get the phone number from the legal notice page
+        num2 = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(
+            (By.XPATH, "//*[contains(text(),'Teléfono')]")
+        )).text.split('Teléfono ')[1].split('.')[0]
+        self.assertEqual(num, num2)
 
+    '''
+    Test for the second case
+    '''
     def test2(self):
         word = 'Faraday'
-        self.browser.get('https://www.innocv.com')
+        # Accept cookies
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.ID, "rcc-decline-button")
         )).click()
 
+        # Go to the contact page
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.LINK_TEXT, "Contacto")
         )).click()
 
-        faraday = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((
-            By.XPATH, f"//*[contains(text(),'{word}')]")))
+        # Get the elements that contain the word 'Faraday' and print it
+        faraday = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located(
+            (By.XPATH, f"//*[contains(text(),'{word}')]")
+        ))
         print(f'Numero de veces que aparece {word}: {len(faraday)}')
 
-
+    '''
+    Test for the third case
+    '''
     def test3(self):
-        self.browser.get('https://www.innocv.com')
+        # Accept cookies
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.ID, "rcc-decline-button")
         )).click()
 
+        # Go to the contact page
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.LINK_TEXT, "Contacto")
         )).click()
 
+        # Get the element of the title form
         enviar = WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "p.MuiTypography-root jss197 MuiTypography-body1".replace(' ', '.'))
+            (By.CSS_SELECTOR, "p.MuiTypography-root" +
+             " jss197 MuiTypography-body1".replace(' ', '.'))
         ))
+        # Move to the title form
         self.browser.execute_script("arguments[0].scrollIntoView();", enviar)
+        # Get the submit button and click it
         enviar = WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "button.MuiButtonBase-root MuiButton-root MuiButton-contained jss200 MuiButton-containedPrimary".replace(' ', '.'))
+            (By.CSS_SELECTOR, "button.MuiButtonBase-root MuiButton-root " +
+             "MuiButton-contained jss200 MuiButton-containedPrimary".replace(' ', '.'))
         ))
         self.browser.execute_script("arguments[0].click();", enviar)
-        error = self.browser.find_element(By.CSS_SELECTOR, 'p.MuiFormHelperText-root jss206 Mui-error'.replace(' ', '.'))
-        error = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((
-            By.XPATH, "//p[text()='Campo requerido']")))
+        # Get the error message
+        error = self.browser.find_element(By.CSS_SELECTOR,
+                                          'p.MuiFormHelperText-root' +
+                                          ' jss206 Mui-error'.replace(' ', '.')
+                                          )
+        error = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+            (By.XPATH, "//p[text()='Campo requerido']")
+        ))
+        # Assert that the error message is not null
         self.assertIsNotNone(error)
 
-
+    '''
+    Test for the fourth case
+    '''
     def test4(self):
-        self.browser.get('https://www.innocv.com')
+        # Accept cookies
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.ID, "rcc-decline-button")
         )).click()
 
+        # Go to the contact page
         WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(
             (By.LINK_TEXT, "Contacto")
         )).click()
 
+        # Get the element of the new's title section and move to it
         noticias = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
             (By.XPATH, "//p[text()='noticias']")
         ))
         self.browser.execute_script("arguments[0].scrollIntoView();", noticias)
 
+        # Wait for the news to be present in the page
         noticias = WebDriverWait(self.browser, 20).until(EC.presence_of_element_located(
             (By.XPATH, "//*[@id='root']/div/div/div[2]/div[2]/div[2]/article")
         ))
-        fechas = WebDriverWait(self.browser, 20).until(EC.presence_of_all_elements_located((
-            By.XPATH, "//*[@id='root']/div/div/div[2]/div[2]/div[2]/article/section[*]/div[2]/div[1]/p[2]")))
+        # Get the news dates
+        fechas = WebDriverWait(self.browser, 20).until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//*[@id='root']/div/div/div[2]/div[2]/div[2]/article/section[*]/div[2]/div[1]/p[2]")
+        ))
 
+        # Assert that the news dates are not older than 2 months
         today = datetime.today()
         for fecha in fechas:
-            f = datetime.strptime(fecha.text,'%d-%m-%Y')
-            self.assertIn((today.year - f.year)*12 + (today.month  - f.month ), [0,1,2])
+            f = datetime.strptime(fecha.text, '%d-%m-%Y')
+            self.assertIn((today.year - f.year)*12 + (today.month-f.month),
+                          [0, 1, 2])
 
 
 if __name__ == '__main__':
